36f9400f09ebc52720338fb29ef023c54f8be732
Jan Abrell <abrell@posteo.de>
2025-10-17 08:28:27 +0200

    main

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from eutl_scraper.extract import extract_accounts, extract_compliance
+from eutl_scraper.extract import (
+    extract_accounts,
+    extract_compliance,
+    extract_installations,
+    extract_transactions,
+)
 
 if __name__ == "__main__":
     df_accounts = extract_accounts()
     df_compliance = extract_compliance()
+    df_installations = extract_installations()
+    df_transactions = extract_transactions()
     print("done")
