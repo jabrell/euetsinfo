e5599a1e33aced9cb419d7d2b554c74154c3daca
Jan Abrell <abrell@posteo.de>
2026-04-09 17:39:56 +0200

    add new transaction extraction

--- eutl_scraper/eutl/extract_new/__init__.py
+++ eutl_scraper/eutl/extract_new/__init__.py
@@ -4,8 +4,14 @@ from eutl_scraper.settings import Settings
 
 from .compliance import extract_compliance
 from .installations import extract_installations
+from .transactions import extract_projects, extract_transactions
 
-__all__ = ["extract_compliance", "extract_installations"]
+__all__ = [
+    "extract_compliance",
+    "extract_installations",
+    "extract_projects",
+    "extract_transactions",
+]
 
 
 def extract_all(dir_data: Path) -> None:
@@ -23,4 +29,12 @@ def extract_all(dir_data: Path) -> None:
     extract_compliance(
         fn_source=settings.fp("compliance", settings.dir_source),
         fn_out=settings.fp("compliance", settings.dir_extracted),
+    )
+    extract_transactions(
+        fn_source=settings.fp("transactions", settings.dir_source),
+        fn_out=settings.fp("transactions", settings.dir_extracted),
+    )
+    extract_projects(
+        fn_source=settings.fp("transactions", settings.dir_source),
+        fn_out=settings.fp("projects", settings.dir_extracted),
     )
